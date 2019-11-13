bf4147e5038fe1f56e3b3e8d099b5dd861060bc0
marthe22 <marthe.boulleau@gmail.com>
2019-11-13 17:48:17 +0000

    adding odrlpanel file

---
+++ simple-app/src/odrlPanel.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ODRLpanel extends Component{
+
+    render(){
+        return(
+            <div className="odrlfile">
+                <h1>ODRL panel</h1>
+                <div>Panel: Panel1</div>
+            </div>
+
+        )
+    }
+
+}
+export default ODRLpanel;
